744d6b381c7e12dcd9f89631fd1c1130470e1086
Brian Romanko <hello@bromanko.com>
2025-08-16 11:33:19 -0700

    Add support for positional holds

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -92,9 +92,9 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_POSITIONAL_COLEMAKDH \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_MT_L(LGUI, A),   U_MT_L(LALT, R),   U_MT_L(LCTRL, S),  U_MT_L(LSHFT, T),  &kp G,             &kp M,             U_MT_R(LSHFT, N),  U_MT_R(LCTRL, E),  U_MT_R(LALT, I),   U_MT_R(LGUI, O),   \
+U_LT_L(U_BUTTON, Z), U_MT_L(RALT, X), &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT_R(RALT, DOT), U_LT_R(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT_L(U_MEDIA, ESC),U_LT_L(U_NAV, SPACE),U_LT_L(U_MOUSE, TAB),U_LT_R(U_SYM, RET),U_LT_R(U_NUM, BSPC),U_LT_R(U_FUN, DEL),U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDHK \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,3 +5,7 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+#define U_MT_L(MOD, TAP) &u_mt_l MOD TAP
+#define U_MT_R(MOD, TAP) &u_mt_r MOD TAP
+#define U_LT_L(LAYER, TAP) &u_lt_l LAYER TAP
+#define U_LT_R(LAYER, TAP) &u_lt_r LAYER TAP
