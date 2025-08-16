aff9dd86f999162cb5dbc0346480a2cae2cf8ba4
Brian Romanko <hello@bromanko.com>
2025-08-16 11:33:19 -0700

    Add positional hold defines for tornblue

--- miryoku/mapping/38/tornblue.h
+++ miryoku/mapping/38/tornblue.h
@@ -16,6 +16,11 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
           XXX  K32  K33  K34       K35  K36  K37  XXX
 
+
+#define KEYS_L K00 K01 K02 K03 K04 K10 K11 K12 K13 K14 K20 K21 K22 K23 K24  // Left-hand keys.
+#define KEYS_R K05 K06 K07 K08 K09 K15 K16 K17 K18 K19 K25 K26 K27 K28 K29  // Right-hand keys.
+#define THUMBS K32 K33 K34 K35 K36 K37                                      // Thumb keys.
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_TORNBLUE
