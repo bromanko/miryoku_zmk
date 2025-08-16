25c9c991310a6807d703cffe482b9355b07d64a0
Brian Romanko <hello@bromanko.com>
2025-08-16 12:23:30 -0700

    Fix key positions for positional hold

--- miryoku/mapping/38/tornblue.h
+++ miryoku/mapping/38/tornblue.h
@@ -17,9 +17,9 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
           XXX  K32  K33  K34       K35  K36  K37  XXX
 
 
-#define KEYS_L K00 K01 K02 K03 K04 K10 K11 K12 K13 K14 K20 K21 K22 K23 K24  // Left-hand keys.
-#define KEYS_R K05 K06 K07 K08 K09 K15 K16 K17 K18 K19 K25 K26 K27 K28 K29  // Right-hand keys.
-#define THUMBS K32 K33 K34 K35 K36 K37                                      // Thumb keys.
+#define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24  // Left-hand keys.
+#define KEYS_R 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29  // Right-hand keys.
+#define THUMBS 30 31 32 33 34 35                        // Thumb keys.
 
 #endif
 
